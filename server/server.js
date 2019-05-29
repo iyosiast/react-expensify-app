48d7e1a745aaa561c888370422c2149b73feb6f5
Iyosias Tessema <iyosias.tessema@uoit.net>
2019-05-29 15:54:00 -0400

    Fixed port number for heroku

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
@@ -10,6 +9,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(port,() => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running');
 });
